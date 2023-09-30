92d98d1fc14b796a486990a3e60798bbe935f45d
soblvsk <SobVitalya@yandex.ru>
2023-09-30 15:34:26 +0700

    feat: create class Crime

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/Crime.kt
@@ -0,0 +1,9 @@
+package com.bignerdranch.android.criminalintent
+
+import java.util.Date
+import java.util.UUID
+
+data class Crime(val id: UUID = UUID.randomUUID(),
+                 var title : String = "",
+                 var date: Date = Date(),
+                 var isSolved: Boolean = false)
