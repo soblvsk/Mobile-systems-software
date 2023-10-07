805e3a4bab120c112720e42b4c0b5c01ce1f76fd
soblvsk <SobVitalya@yandex.ru>
2023-10-07 21:34:14 +0700

    style: add spaces

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDatabase.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeDatabase.kt
@@ -7,6 +7,7 @@ import com.bignerdranch.android.criminalintent.Crime
 
 @Database(entities = [Crime::class], version = 1)
 @TypeConverters(CrimeTypeConverters::class)
-abstract class CrimeDatabase: RoomDatabase() {
+abstract class CrimeDatabase : RoomDatabase() {
+
     abstract fun crimeDao(): CrimeDao
 }
