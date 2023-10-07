15fae46738c92280da930b94f6965e9cb301afa2
soblvsk <SobVitalya@yandex.ru>
2023-10-07 21:30:14 +0700

    fix: change types in typeConverter

--- CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeTypeConverters.kt
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/database/CrimeTypeConverters.kt
@@ -4,24 +4,29 @@ import androidx.room.TypeConverter
 import java.util.Date
 import java.util.UUID
 
+
 class CrimeTypeConverters {
+
     @TypeConverter
-    fun fromDate(date: Date): Long {
-        return date.time
+    fun fromDate(date: Date?): Long? {
+        return date?.time
     }
 
     @TypeConverter
-    fun toDate(millisSinceEpoch: Long): Date {
-        return Date(millisSinceEpoch)
+    fun toDate(millisSinceEpoch: Long?): Date? {
+        return millisSinceEpoch?.let {
+            Date(it)
+        }
     }
 
     @TypeConverter
-    fun toUUID(uuid: String?) : UUID? {
+    fun toUUID(uuid: String?): UUID? {
         return UUID.fromString(uuid)
     }
 
     @TypeConverter
-    fun fromUUID(uuid: String?) : String? {
+    fun fromUUID(uuid: UUID?): String? {
         return uuid?.toString()
     }
+
 }
