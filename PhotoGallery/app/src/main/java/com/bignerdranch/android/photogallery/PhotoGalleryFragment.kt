96ac2a837036e2ef682b3ec2c5ab78952222181c
soblvsk <SobVitalya@yandex.ru>
2023-10-19 11:04:49 +0700

    feat: executing a web request

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryFragment.kt
@@ -1,6 +1,7 @@
 package com.bignerdranch.android.photogallery
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -8,8 +9,13 @@ import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.bignerdranch.android.photogallery.api.FlickrApi
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.scalars.ScalarsConverterFactory
+
+private const val TAG = "PhotoGalleryFragment"
 
 class PhotoGalleryFragment : Fragment() {
 
@@ -21,6 +27,18 @@ class PhotoGalleryFragment : Fragment() {
         val retrofit: Retrofit = Retrofit.Builder().baseUrl("https://www.flickr.com/")
             .addConverterFactory(ScalarsConverterFactory.create()).build()
         val flickrApi: FlickrApi = retrofit.create(FlickrApi::class.java)
+
+        val flickrHomePageRequest: Call<String> = flickrApi.fetchContents()
+
+        flickrHomePageRequest.enqueue(object : Callback<String> {
+            override fun onFailure(call: Call<String>, t: Throwable) {
+                Log.e(TAG, "Failed to fetch photos", t)
+            }
+
+            override fun onResponse(call: Call<String>, response: Response<String>) {
+                Log.d(TAG, "Response received:${response.body()}")
+            }
+        })
     }
 
     override fun onCreateView(
--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/api/FlickrApi.kt
@@ -5,5 +5,5 @@ import retrofit2.http.GET
 
 interface FlickrApi {
     @GET("/")
-    fun fetchPhotos(): Call<String>
+    fun fetchContents(): Call<String>
 }
