911a32ffd1177717910fdaef222e08d9005e9e8c
soblvsk <SobVitalya@yandex.ru>
2023-10-21 15:26:34 +0700

    style: add a space

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/PhotoGalleryActivity.kt
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class PhotoGalleryActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_photo_gallery)
