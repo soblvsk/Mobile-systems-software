2cfcdd1b493d77e6758cdf5dc5a7eaaf4d7ff10a
soblvsk <SobVitalya@yandex.ru>
2023-12-13 20:07:40 +0700

    chore: change DB files: quey fun getAll

--- ToDoList/app/src/main/java/com/bignerdranch/android/todolist/database/ToDoListDao.kt
+++ ToDoList/app/src/main/java/com/bignerdranch/android/todolist/database/ToDoListDao.kt
@@ -1,6 +1,5 @@
 package com.bignerdranch.android.todolist.database
 
-import androidx.lifecycle.LiveData
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
@@ -10,8 +9,8 @@ import com.bignerdranch.android.todolist.ToDoList
 @Dao
 interface ToDoListDao {
 
-    @Query("SELECT * FROM ToDoList ORDER BY priority DESC")
-    fun getAll(): LiveData<List<ToDoList>>
+    @Query("SELECT * FROM ToDoList ORDER BY priority ASC")
+    fun getAll(): List<ToDoList>
 
     @Insert
     fun addTask(task: ToDoList)
--- ToDoList/app/src/main/java/com/bignerdranch/android/todolist/database/ToDoListDatabase.kt
+++ ToDoList/app/src/main/java/com/bignerdranch/android/todolist/database/ToDoListDatabase.kt
@@ -4,7 +4,6 @@ import androidx.room.Database
 import androidx.room.RoomDatabase
 import com.bignerdranch.android.todolist.ToDoList
 
-
 @Database(entities = [ToDoList::class], version = 1)
 abstract class ToDoListDatabase : RoomDatabase() {
     abstract fun toDoListDao(): ToDoListDao
